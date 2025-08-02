af039832aa2f453448c199008b153ec3500f5d21
sgallo <sofiagdelarosa@gmail.com>
2025-08-02 15:23:13 -0500

    mi primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    #return render(request, 'home.html')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Sofia Gallo de la Rosa'})
+
+def about(request):
+    return HttpResponse('<h1>About Us</h1>')
